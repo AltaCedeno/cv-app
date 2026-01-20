54d8f07bc501b6c34f1152a0c43cc4fda6dc746a
ALTA <codatasweb@gmail.com>
2026-01-20 11:14:52 -0400

    Configurar despliegue estático para GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -14,7 +14,7 @@ import type { NextConfig } from "next";
 ///** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // Indica que genere HTML estático
-  basePath: '/cv-alta', // Reemplaza con el nombre exacto de tu repositorio
+  basePath: '/cv-app', // Reemplaza con el nombre exacto de tu repositorio
   images: {
     unoptimized: true, // GitHub Pages no soporta la optimización de imágenes de Next.js
   },
